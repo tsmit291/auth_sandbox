7376ee9b9c9cdc6da4552a4084c4f1c132a9e3dd
= <tsmit291@icloud.com>
2016-01-27 14:48:42 -0700

    still need to make only logged in users see the ticket page

--- routes/auth.js
+++ routes/auth.js
@@ -3,24 +3,33 @@ var router = express.Router();
 var bcrypt = require('bcrypt')
 
 router.get('/', function(req, res, next) {
-    res.redirect("/tickets");
+  if (req.cookies.user){
+    res.redirect('/tickets')
+  } else
+    res.render('auth/signin', {button_text: 'please sign in'})
 });
 
+/*Check if user is already signed in- if yes, render yougood, otherwise redirect to sign in*/
 router.get('/signin', function(req, res, next) {
-
-
+    if(req.cookies.user){
+      res.redirect('/tickets');
+    } else {
       res.render("auth/signin", {button_text: "sign in"});
-
+    };
 });
 
+/*When a user signs out, clear the user cookie from our db*/
 router.get('/signout', function(req, res, next) {
+    res.clearCookie('users');
     res.render("auth/signin", {button_text: "sign in", notice: "you have been signed out"});
 });
 
+/*When a user signs up, add the user cookie to our db*/
 router.get('/signup', function(req, res, next) {
     res.render("auth/signup", {button_text: "sign up"});
 });
 
+/*If you don't exist in the db, tell the user you aren't authorized*/
 router.get('/no_auth', function(req, res, next) {
     res.render("auth/yousuck");
 });
--- routes/users.js
+++ routes/users.js
@@ -7,19 +7,22 @@ var bcrypt = require('bcrypt')
 function Users(){
   return knex('users');
 }
-
+/*Here someone is signing in-*/
 router.post('/', function(req, res, next) {
   Users().insert(req.body).then(function(val){
+    res.cookie('users', req.body.email);
     res.redirect("/tickets");
   });
 });
 
+/*Here someone is signing up*/
 router.post('/login', function(req, res, next) {
     Users().where({email: req.body.email, password: req.body.password}).first().then(function(found){
        if (found){
+         res.cookie('users', req.body.email);
          res.redirect("/tickets");
        } else {
-         res.redirect("/no_auth");
+         res.render("auth/yousuck");
        }
     })
 });
